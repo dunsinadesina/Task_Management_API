935113ca21c22456e2987bf8f39928f49c4bb0b6
dunsinadesina <adesinajesudunsin@gmail.com>
2024-10-27 15:57:35 +0100

    backend update

--- firebase.ts
+++ firebase.ts
@@ -5,10 +5,26 @@ import { getAuth as getClientAuth } from "firebase/auth"; // For client-side aut
 
 dotenv.config();
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}') as {
+  projectId: string;
+  clientEmail: string;
+  privateKey: string;
+};
+
+if (!serviceAccount.projectId) {
+    throw new Error("Missing project_id in service account credentials");
+}
+
+// Check if all required variables are defined
+if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error("Missing required Firebase environment variables");
+}
+
+
 // Initialize Firebase Admin SDK (for server-side)
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
