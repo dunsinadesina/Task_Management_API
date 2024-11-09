321b5e629e22833a78133c49093a4539d232f29a
dunsinadesina <adesinajesudunsin@gmail.com>
2024-11-09 17:40:13 +0100

    backend update

--- app.ts
+++ app.ts
@@ -49,7 +49,7 @@ app.get('/auth/google/failure', (req: Request, res: Response) => {
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(cors({
-    origin: 'https://taskify-lac-beta.vercel.app/',
+    origin: 'https://taskify-lac-beta.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: false,
 }));
