1810ab53579ca454a6936074730f4f4e17765ede
dunsinadesina <adesinajesudunsin@gmail.com>
2024-11-09 21:33:03 +0100

    backend update

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -248,7 +248,7 @@ export const forgotPassword = async (req: Request, res: Response) => {
 
 export const resetPassword = async (req: Request, res: Response) => {
     const { newPassword } = req.body;
-    const token = String(req.query.token)
+    const token = req.params;
 
     console.log('Received reset token:', token);
 
